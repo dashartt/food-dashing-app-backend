50e3281b25080c8475b44c72ea3b0c6f321a33d4
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-01-23 05:54:14 -0300

    chore: :fire: remove mongodb connection

--- src/start/server.ts
+++ src/start/server.ts
@@ -1,30 +1,27 @@
 import 'dotenv/config'
 import { connect as connectDb } from 'mongoose'
 import app from './app'
-// import serverIo from '../start/io'
+import serverIo from '../start/io'
 
 const appPort = parseInt(process.env.APP_PORT as string) as number
-// const socketPort = parseInt(process.env.SOCKET_PORT as string) as number
+const socketPort = parseInt(process.env.SOCKET_PORT as string) as number
 const mongoUrl = process.env.MONGO_URL as string
 
-connectDb(mongoUrl)
-  .then(() => {
-    console.log("\x1b[33m%s\x1b[0m', `=> 🚀 Database connected")
+// connectDb(mongoUrl)
+//   .then(() => {
+console.log("\x1b[33m%s\x1b[0m', `=> 🚀 Database connected")
 
-    app.listen(appPort, () => {
-      console.log(
-        '\x1b[33m%s\x1b[0m',
-        `=> Server running on the port: ${appPort}`
-      )
-    })
+app.listen(appPort, () => {
+  console.log('\x1b[33m%s\x1b[0m', `=> Server running on the port: ${appPort}`)
+})
 
-    // serverIo.listen(socketPort, () =>
-    //   console.log(
-    //     '\x1b[33m%s\x1b[0m',
-    //     `=> Socket-io running on the port: ${socketPort}`
-    //   )
-    // )
-  })
-  .catch(() => {
-    console.warn('Database and Server not connected')
-  })
+serverIo.listen(socketPort, () =>
+  console.log(
+    '\x1b[33m%s\x1b[0m',
+    `=> Socket-io running on the port: ${socketPort}`
+  )
+)
+// })
+// .catch(() => {
+//   console.warn('Database and Server not connected')
+// })
