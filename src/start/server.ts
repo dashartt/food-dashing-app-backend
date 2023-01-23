32a798ef8edef61a96b9730ce16f6929b08420d6
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-01-23 15:24:00 -0300

    fix: :adhesive_bandage: try to fix web container has stopped

--- src/start/server.ts
+++ src/start/server.ts
@@ -3,22 +3,25 @@ import { connect as connectDb } from 'mongoose'
 import app from './app'
 import serverIo from '../start/io'
 
-const PORT = parseInt(process.env.PORT as string) as number
-const MONGO_UL = process.env.MONGO_URL as string
+const appPort = parseInt(process.env.PORT as string) as number
+const socketPort = parseInt(process.env.SOCKET_PORT as string) as number
+const mongoUrl = process.env.MONGO_URL as string
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log('\x1b[33m%s\x1b[0m', `=> Server running on the port: ${PORT}`)
+app.listen(appPort, '0.0.0.0', () => {
+  console.log('\x1b[33m%s\x1b[0m', `=> Server running on the port: ${appPort}`)
 })
 
-serverIo.listen(PORT, () =>
-  console.log('\x1b[33m%s\x1b[0m', `=> Socket-io running on the port: ${PORT}`)
+serverIo.listen(socketPort, () =>
+  console.log(
+    '\x1b[33m%s\x1b[0m',
+    `=> Socket-io running on the port: ${socketPort}`
+  )
 )
 
-connectDb(MONGO_UL)
+connectDb(mongoUrl)
   .then(() => console.log('\x1b[33m%s\x1b[0m', `=> Database connected`))
   .catch(() => {
     console.log('\x1b[33m%s\x1b[0m', `=> Database not connected`)
-
     console.log('\x1b[33m%s\x1b[0m', `=> Server and socket io will close`)
     process.exit(0)
   })
