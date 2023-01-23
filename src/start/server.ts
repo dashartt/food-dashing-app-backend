68bfd100654fff49a17e8a6e379bbc5aefe20ca4
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-01-23 15:07:23 -0300

    refactor: :recycle: express and socketio starting together

--- src/start/server.ts
+++ src/start/server.ts
@@ -1,18 +1,21 @@
 import 'dotenv/config'
 import { connect as connectDb } from 'mongoose'
-import app from './app'
-import serverIo from '../start/io'
+// import app from './app'
+import expressWithSocket from '../start/io'
 
 const PORT = parseInt(process.env.PORT as string) as number
 const MONGO_UL = process.env.MONGO_URL as string
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log('\x1b[33m%s\x1b[0m', `=> Server running on the port: ${PORT}`)
+expressWithSocket.listen(PORT, '0.0.0.0', () => {
+  console.log(
+    '\x1b[33m%s\x1b[0m',
+    `=> Server and SocketIO running on the port: ${PORT}`
+  )
 })
 
-serverIo.listen(PORT, () =>
-  console.log('\x1b[33m%s\x1b[0m', `=> Socket-io running on the port: ${PORT}`)
-)
+// serverIo.listen(PORT, () =>
+//   console.log('\x1b[33m%s\x1b[0m', `=> Socket-io running on the port: ${PORT}`)
+// )
 
 connectDb(MONGO_UL)
   .then(() => console.log('\x1b[33m%s\x1b[0m', `=> Database connected`))
