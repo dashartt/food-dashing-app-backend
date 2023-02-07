bee234f40eeba2618b5c029c493fcaebf7265165
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-07 18:40:20 -0300

    refactor: :fire: remove unused files

--- src/routes/client.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { addClient } from '../controllers/client.controller'
-
-const router = Router()
-
-router.post('/', addClient)
-
-export default router
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import itemCategoryRouter from '../routes/itemCategory.routes'
 import menuItemRouter from '../routes/menuItem.routes'
 import addressRouter from '../routes/address.routes'
-import clientRouter from '../routes/client.routes'
 import orderRouter from '../routes/order.routes'
 import accountRouter from './account.routes'
 
@@ -11,7 +10,6 @@ const router = Router()
 router.use('/category', itemCategoryRouter)
 router.use('/menu', menuItemRouter)
 router.use('/address', addressRouter)
-router.use('/client', clientRouter)
 router.use('/orders', orderRouter)
 router.use('/account', accountRouter)
 
