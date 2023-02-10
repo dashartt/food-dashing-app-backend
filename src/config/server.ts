b89e892ea7e3328e26d667581e62a5e540e553d0
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-10 13:49:05 -0300

    fix: :adhesive_bandage: try to fix preflight error

--- src/config/server.ts
+++ src/config/server.ts
@@ -29,18 +29,17 @@ export const beamsClient = new PushNotifications({
 app.use(express.json())
 app.use(
   cors({
-    origin: '*',
+    origin: FRONTEND_URL,
+    preflightContinue: true,
   })
 )
 app.use(express.urlencoded({ extended: false }))
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', '*')
+//   res.header('Access-Control-Allow-Credentials', 'true')
+//   next()
+// })
 
 // Routes ----------------------->
 app.use(routes)
