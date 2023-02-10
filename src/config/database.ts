cc801397467a0e44c839ccc656996aa8ca8b994e
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-10 13:24:46 -0300

    fix: :adhesive_bandage: mongoose warning

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,13 @@
 import { ServerApiVersion } from 'mongodb'
-import { connect } from 'mongoose'
+import mongoose from 'mongoose'
 
 const MONGO_URL =
   process.env.MONGO_URL ||
   'mongodb+srv://dashart:7CB19066@cluster0.bhvn9ws.mongodb.net/?retryWrites=true&w=majority'
 
-const mongodb = connect(MONGO_URL, {
+mongoose.set('strictQuery', false)
+
+const mongodb = mongoose.connect(MONGO_URL, {
   serverApi: ServerApiVersion.v1,
 })
 
