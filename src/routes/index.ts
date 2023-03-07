0a0c7d10481c49497be9324c9cb2f676e2dc2899
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-03-07 04:55:09 -0300

    revert: :rewind: rollback to commit #2dd60c1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,6 @@ import addressRouter from '../routes/address.routes'
 import orderRouter from '../routes/order.routes'
 import accountRouter from './account.routes'
 import additionalRouter from './additional.routes'
-import wppWebhookRouter from './wppWebhook.routes'
 
 const router = Router()
 
@@ -15,6 +14,5 @@ router.use('/address', addressRouter)
 router.use('/orders', orderRouter)
 router.use('/account', accountRouter)
 router.use('/additional', additionalRouter)
-router.use('/wpp-webhook', wppWebhookRouter)
 
 export default router
