a2f0d183f14df59bc34cf0e5f3f34b4c043ade4c
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-03-08 13:12:19 -0300

    feat: :sparkles: returns with Beam notification

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,7 +3,7 @@ import * as orderRepository from '../repositories/order.repository'
 
 import { IOrder } from '../types'
 import { pusher } from '../config/server'
-// import { notifyNewOrder, notifyUpdateOrderStatus } from '../events'
+import { notifyNewOrder, notifyUpdateOrderStatus } from '../events'
 import { isObjectIdOrHexString } from 'mongoose'
 
 export const updateOrderStatus = async (
@@ -17,7 +17,7 @@ export const updateOrderStatus = async (
   await orderRepository.updateOrderStatus(_id, status)
 
   pusher.trigger('client', 'update-order-status', status)
-  // notifyUpdateOrderStatus(status, _id)
+  notifyUpdateOrderStatus(status, _id)
   res.status(200).end()
 }
 
@@ -82,7 +82,7 @@ export const addOrder = async (
   if (!orderId) return next(0)
 
   pusher.trigger('admin', 'new-order', orderId)
-  // notifyNewOrder()
+  notifyNewOrder()
 
   res.status(200).json({ orderId })
 }
